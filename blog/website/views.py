3c132b7899ad320a12ef98a363917cb525da0d29
VictorManuelChang <victormanuelchang@gmail.com>
2024-10-15 11:17:59 -0400

    Renderização e arquivos de mídia

--- blog/website/views.py
+++ blog/website/views.py
@@ -1,6 +1,17 @@
 from django.shortcuts import render
+from .models import Post
 
 def hello_blog(request):
     lista = ['Django', 'Python', 'Git', 'Html', 'Banco de Dados']
-    data = {'name' : 'Curso de Django 3', 'lista_tecnologia' : lista}
+    
+    list_posts = Post.objects.filter(deleted = False)
+    
+    data = {'name' : 'Curso de Django 3',
+            'lista_tecnologia' : lista,
+            'posts': list_posts}
+
     return render(request, 'index.html', data)
+
+def post_detail(request, id):
+    post = Post.objects.get(id=id)
+    return render(request, 'post_detail.html', {'post': post})
